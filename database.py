406df8c5ef7d017023438f8bfc3c57b1d6333b71
Leonard <leo.brauneis@gmx.de>
2025-05-08 10:59:59 +0200

    daten verschlüsselt

--- database.py
+++ database.py
@@ -1,24 +1,28 @@
-import mariadb, sys
+import os, mariadb, sys
+from dotenv import load_dotenv, dotenv_values
 
 
 class Datenbank():
     def __init__(self):
         nummer = 0
 
-try:#connect
-    conn = mariadb.connect(
-        user = "team08",
-        password = "8US9V",
-        host = "10.80.0.206",
-        port = 3306,
-        database = "team08")
- 
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB PLatform: {e}")
-    sys.exit(1)
-#get cursor
-cur = conn.cursor()
-#"SELECT artikel.Artikelname, artikel.Preis_Netto, artikel.Lagerbestand, lieferant.Lieferantenname FROM artikel INNER JOIN lieferant ON artikel.Lieferant = lieferant.ID_Lieferant;"
-cur.execute(
-    "SELECT * FROM anrede WHERE anrede = 'divers';"
-    )
+        load_dotenv()
+
+        try:#connect
+            conn = mariadb.connect(
+                user = os.getenv("USER"),
+                password = os.getenv("PASSWORD"),
+                host = os.getenv("HOST"),
+                port = os.getenv("PORT"),
+                database = os.getenv("DATABASE")
+            )
+
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB PLatform: {e}")
+            sys.exit(1)
+        #get cursor
+        cur = conn.cursor()
+        #"SELECT artikel.Artikelname, artikel.Preis_Netto, artikel.Lagerbestand, lieferant.Lieferantenname FROM artikel INNER JOIN lieferant ON artikel.Lieferant = lieferant.ID_Lieferant;"
+        cur.execute(
+            "SELECT * FROM anrede WHERE anrede = 'divers';"
+            )
