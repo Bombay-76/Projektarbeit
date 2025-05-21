c0ee4efd5b5d22f02e9316e5ddb84b0afabbed28
Bombay-76 <lennie2502@hotmail.de>
2025-05-21 10:26:26 +0200

    bugfix 21.05.2025

--- database.py
+++ database.py
@@ -7,9 +7,9 @@ from GUI import *
 
 class Datenbank:
     def __init__(self):
-        load_dotenv()
+        load_dotenv()#schütztdenzugangZurDatenbank
         try:
-            #DatenbankConnection
+            #DatenbankConnection    #
             self.conn = mariadb.connect(
                 user=os.getenv("USER"),
                 password=os.getenv("PASSWORD"),
@@ -23,10 +23,15 @@ class Datenbank:
             print("Da hat etwas nicht geklappt!")
 
 def register(self, mitarbeiter_nr, vorname, nachname, adresse, passwort):
+
+    vorname = tb_vorname().get()
+    nachname = tb_nachname().get()#ValueAusDenTextBoxenHolen
+    adresse = tb_adresse().get()
+
     try:
         hashed_pw = hashlib.sha256(passwort.encode()).hexdigest()
         self.cur.execute(
-            "INSERT INTO mitarbeiter (mitarbeiter_nr, vorname, nachname, adresse, passwort) VALUES (%, ?, ?, ?)",
+            "INSERT INTO mitarbeiter (mitarbeiter_nr, vorname, nachname, adresse, passwort) VALUES (?, ?, ?, ?, ?)",
             (mitarbeiter_nr, vorname, nachname, adresse, hashed_pw)
         )
         self.conn.commit()
@@ -36,14 +41,14 @@ def register(self, mitarbeiter_nr, vorname, nachname, adresse, passwort):
     
 def login(self, mitarbeiter_nr, passwort):
 
-    login_data = tb_ID().get()
-
+    mitarbeiter_nr = tb_ID().get()
+    passwort = tb_passwort().get()
 
     try:
         hashed_pw = hashlib.sha256(passwort.encode()).hexdigest()
         self.cur.execute(
-            "SELECT * FROM mitarbeiter WHERE mitarbeiter_nr = ? AND passwort = ?",
-            (login_data, hashed_pw)
+            "SELECT * FROM mitarbeiter WHERE mitarbeiter_nr = ? AND passwort = ?",#FürhtSQLCodeAus
+            (mitarbeiter_nr, hashed_pw)
         )
         return self.cur.fetchone()
     except mariadb.Error:
@@ -51,8 +56,27 @@ def login(self, mitarbeiter_nr, passwort):
     
 def insight(self):
     try:
-        self.cur.execute("SELECT * FROM Zeiterfassung")
+        self.cur.execute("SELECT * FROM Zeiterfassung")#FürhtSQLCodeAus
         daten = self.cur.fetchall()
         return daten
     except mariadb.Error:
         print("Da hat etwas nicht geklappt!")
+
+def add(self, projekt_id, projektname, kunden_nr):#FunktionZumAnlegenNeuerProjekte
+
+    projekt_id = tb_projekt_id().get()#ValuesVonTextBoxenHolen
+    projektname = tb_projektname().get()
+    kunden_nr = tb_projektname().get()
+
+    try:
+        self.cur.execute(#Ausführenvonsqlcode
+        "INSERT INTO Projekt (projekt_id, projektname, Kunden_nr) VALUES (?, ?, ?)"
+    )
+    except mariadb.Error:
+        print("Da hat etwas nicht geklappt")
+
+def delete():
+    
+
+def close(self):#FunktionZumSchließenDerDatenbankConnection
+    self.conn.close()
